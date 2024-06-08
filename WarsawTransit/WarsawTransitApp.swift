feda4020aef5e186cff10850b1e916cba7b730fb
Jacob <69864916+Jacobinoo@users.noreply.github.com>
2024-06-08 16:41:23 +0200

    Initial commit

---
+++ WarsawTransit/WarsawTransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarsawTransitApp.swift
+//  WarsawTransit
+//
+//  Created by Jakub on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WarsawTransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
